3e91507554e72bad64a1668969e9a3d8606617a8
IanSilvaCertacon <ian.silva@certacon.com.br>
2024-11-27 17:09:04 -0300

    atualizacao books

---
+++ src/main/resources/db/migration/V2__Insert_Data_In_Person.sql
@@ -0,0 +1,5 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `last_name`) VALUES
+(1,'São Paulo','Ayrton','Male','Senna'),
+(2,'Anchiano sim - Italy','Leonardo','Male','Da Vinci'),
+(4,'Porbandar - India','Indira','Female','Gandhi'),
+(5,'Porbandar - India','Indira','Female','Gandhi');
